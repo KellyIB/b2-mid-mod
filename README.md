0767cf59a784aadd43a2fdd5ad00eb596b12a450
KellyIB <kellyibard@gmail.com>
2020-02-14 09:39:22 -0700

    set up one to many relationship between studio and movie.

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ Base repository for B2 assessments and diagnostics.
 This repository requires and has been tested on Ruby v2.5.3 and is based on Rails 5.1.7.
 
 RSpec and Shoulda-Matchers have been installed and set up.
-
-
---
+++ spec/model/movie_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Movie, type: :model do
+
+  describe "validations" do
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :creation_year}
+  it {should validate_presence_of :genre}
+end
+
+  describe "relationships" do
+    it {should belong_to :studio}
+    it {should have_many :actors}
+  end
+
+
+
+end
